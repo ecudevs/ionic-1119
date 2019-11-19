5357b4eeae0273aa2f475363f95003b331fd6f4b
Thian Lopez <thianlopezz@gmail.com>
2019-11-18 20:59:34 -0500

    4 dia

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { ModalController } from "@ionic/angular";
 import { ProductoDetalleComponent } from "../producto/producto-detalle/producto-detalle.component";
 import { CarritoComponent } from "../producto/carrito/carrito.component";
+import { ProductoService } from "../services/producto.service";
 
 @Component({
   selector: "app-tab1",
@@ -9,55 +10,7 @@ import { CarritoComponent } from "../producto/carrito/carrito.component";
   styleUrls: ["tab1.page.scss"]
 })
 export class Tab1Page {
-  productos = [
-    {
-      producto: "Nylon Braided Lightning to USB A Cable",
-      categorias: [{ _id: "COMPUTER", categoria: "Computer" }],
-      descripcion:
-        "Apple MFi certified charging and syncing cable for your Apple devices",
-      fotos: [
-        "https://images-na.ssl-images-amazon.com/images/I/81%2BF83td0DL._SL1500_.jpg",
-        "https://images-na.ssl-images-amazon.com/images/I/41cRjgok0ML.jpg"
-      ],
-      precio: 219.999,
-      stock: 5,
-      feCreacion: new Date()
-    },
-    {
-      producto: "Wemo Light Switch",
-      categorias: [
-        { _id: "GOOGLE", categoria: "Google assitance" },
-        { _id: "HOME", categoria: "Home" }
-      ],
-      descripcion:
-        "Wemo Light Switch, WiFi enabled, Works with Alexa and the Google Assistant (F7C030fc)",
-      fotos: [
-        "https://images-na.ssl-images-amazon.com/images/I/510yp4Dw5pL._SL1000_.jpg",
-        "https://images-na.ssl-images-amazon.com/images/I/51Mm%2BddYqUL._SL1000_.jpg",
-        "https://images-na.ssl-images-amazon.com/images/I/61gbjA4r5ZL._SL1000_.jpg"
-      ],
-      precio: 34.99,
-      stock: 10,
-      feCreacion: new Date()
-    },
-    {
-      producto: "Apple iPhone 11",
-      categorias: [
-        { _id: "PHONE", categoria: "Phone" },
-        { _id: "APPLE", categoria: "Apple" }
-      ],
-      descripcion:
-        "Carrier - This phone is locked to simple Mobile from Tracfone, which means this Device can only be used on the Simple Mobile wireless network.",
-      fotos: [
-        "https://images-na.ssl-images-amazon.com/images/I/71iO2R%2BCLjL._SL1500_.jpg",
-        "https://images-na.ssl-images-amazon.com/images/I/719BufuTjsL._SL1500_.jpg",
-        "https://images-na.ssl-images-amazon.com/images/I/81LoEmP6IyL._SL1500_.jpg"
-      ],
-      precio: 699.0,
-      stock: 25,
-      feCreacion: new Date()
-    }
-  ];
+  productos: any = [];
 
   productosFiltrados = [];
 
@@ -65,8 +18,16 @@ export class Tab1Page {
 
   cantidadItems = 0;
 
-  constructor(public modalController: ModalController) {
-    this.productosFiltrados = this.productos;
+  constructor(
+    private productoService: ProductoService,
+    public modalController: ModalController
+  ) {}
+
+  ionViewDidEnter() {
+    this.productoService.getProductos().subscribe((response: any) => {
+      this.productos = response.productos;
+      this.productosFiltrados = this.productos;
+    });
   }
 
   filtrar(e) {
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ProductoService } from "../services/producto.service";
+import { AlertController, LoadingController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  selector: "app-tab2",
+  templateUrl: "tab2.page.html",
+  styleUrls: ["tab2.page.scss"]
 })
 export class Tab2Page {
+  constructor(
+    private productoService: ProductoService,
+    public alertController: AlertController,
+    public loadingController: LoadingController
+  ) {}
 
-  constructor() {}
+  async guardarProducto(producto) {
+    const loading = await this.loadingController.create({
+      spinner: "crescent",
+      message: "Por favor espere...",
+      translucent: true,
+      cssClass: "custom-class custom-loading" // podemos configurar estilos de clase
+    });
+    await loading.present();
 
+    this.productoService
+      .insertarProducto({ ...producto, fotos: [producto.foto] })
+      .subscribe(
+        (response: any) => {
+          loading.dismiss();
+          this.showAlert("Guardado");
+        },
+        error => {
+          loading.dismiss();
+          this.showAlert(error.message);
+        }
+      );
+  }
+
+  async showAlert(mensaje) {
+    const alert = await this.alertController.create({
+      header: "Exito",
+      subHeader: "Subtitulo",
+      message: mensaje,
+      buttons: ["OK"]
+    });
+
+    await alert.present();
+  }
 }
